939acec07aba0809685cdf1f703ce32c1665a06a
operatorx <nlpxucan@163.com>
2023-11-07 15:02:00 +0800

    Add files via upload

---
+++ Evol_Instruct/breadth.py
@@ -0,0 +1,14 @@
+base_instruction = "I want you act as a Prompt Creator.\r\n\
+Your goal is to draw inspiration from the #Given Prompt# to create a brand new prompt.\r\n\
+This new prompt should belong to the same domain as the #Given Prompt# but be even more rare.\r\n\
+The LENGTH and complexity of the #Created Prompt# should be similar to that of the #Given Prompt#.\r\n\
+The #Created Prompt# must be reasonable and must be understood and responded by humans.\r\n\
+'#Given Prompt#', '#Created Prompt#', 'given prompt' and 'created prompt' are not allowed to appear in #Created Prompt#\r\n"
+
+
+
+def createBreadthPrompt(instruction):
+	prompt = base_instruction
+	prompt += "#Given Prompt#: \r\n {} \r\n".format(instruction)
+	prompt += "#Created Prompt#:\r\n"
+	return prompt
